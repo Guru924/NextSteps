9a17b490d909639b387be712d29fe6ee34a608bf
Guru924 <guruprasaddas924@gmail.com>
2024-09-29 10:35:21 +0530

    Initial commit: Setup Next.js project using create-next-app.

---
+++ 01intro/app/page.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <h1 className="text-xl">
+      <ul>
+        <li>full stack solution</li>
+        <li>File base Routing</li>
+        <li>Additional features like router from next/navigation</li>
+        <li>optimized rendering</li>
+      </ul>
+    </h1>
+    </>
+  );
+}
